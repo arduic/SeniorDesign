ba4dca6ddb80ca2f3e572b8798ba55dceafbd5a1
Pi Joules <pijoules@gmail.com>
2017-03-13 12:58:54 -0400

    stuff

--- FFT/scripts/signal_from_range_velocity.m
+++ FFT/scripts/signal_from_range_velocity.m
@@ -7,19 +7,13 @@ R = 500; vr = convvel(100, 'mph', 'm/s');  % close, fast towards
 % R = 500; vr = convvel(-100, 'mph', 'm/s');  % close, fast away
 % R = 3000; vr = convvel(-100, 'mph', 'm/s');
 
-<<<<<<< HEAD
+
+
 Fs = 10^6;
 Tm = 10^-3;
 c = 3*10^8;  % speed of light
 df = 10^6;  % beat (delata freq)
 fm = 1/Tm;  % modulation rate (period)
-=======
-c = 2*10^8;  % speed of light
-Tm = 10^-6;
-df = 10^6;  % beat (delata freq)
-% fm = 1/Tm;  % modulation rate (period)
-fm = 1000;
->>>>>>> 80b476235d74b8b5a1b48e6ef9445fc3e6de4734
 f0 = 80*10^9;  % Starting freqency
 % f0 = 3.3*10^9;
 
@@ -39,29 +33,6 @@ elseif vr < 0
 end
 
 % Create signal
-<<<<<<< HEAD
-delay = Tm/10;
-t1 = 0:1/Fs:(Tm-delay);
-t2 = (Tm-delay):1/Fs:Tm;
-
-signal1 = sin(2*pi*fb_up*t1);
-signal2 = sin(2*pi*fb_down*t2);
-
-figure;
-plot(t1, signal1, t2, signal2);
-
-% FFT
-signal = [signal1 signal2];
-L = length(signal);
-Y = fft(signal);
-f = Fs/L*(0:L-1);
-
-figure;
-% semilogx(f, abs(Y));
-semilogy(f, abs(Y));
-% plot(f, abs(Y));
-=======
-Fs = 10^7;
 delay = Tm/6;
 t1 = 0:1/Fs:(Tm-delay);
 t2 = (Tm-delay):1/Fs:Tm;
@@ -80,5 +51,5 @@ f = Fs/L*(0:(L-1));
 
 figure;
 % semilogx(f, abs(Y));
-plot(f, abs(Y));
->>>>>>> 80b476235d74b8b5a1b48e6ef9445fc3e6de4734
+semilogy(f, abs(Y));
+% plot(f, abs(Y));
