82b4bbcd80735bd264cde54b3182911b99eb287c
arduic <patcc2@gmail.com>
2017-05-01 13:18:05 -0400

    Delete ResultsFromTest.m

--- Resolution Calculations/ResultsFromTest.m
+++
@@ -1,8 +0,0 @@
-diffs=res(2:end)-res(1:end-1);  %Get delta of angles
-B = diffs > 0.010001;   %Remove ones where there was no gap
-
-realDiffs = diffs(B);   %Only look at those without gaps
-
-mean(realDiffs) %Calc standard stuff
-median(realDiffs)
-max(realDiffs)
